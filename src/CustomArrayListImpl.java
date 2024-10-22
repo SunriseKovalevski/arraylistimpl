0d5b472db47ae02efe18df92e38d3f869a3b2bb9
Vladislav Kovalevski <SunriseKovalevsky283@gmail.com>
2024-10-22 17:17:03 +0300

    Added impl for add and get methods

--- src/CustomArrayListImpl.java
+++ src/CustomArrayListImpl.java
@@ -1,9 +1,33 @@
 import java.util.Iterator;
-
 public class CustomArrayListImpl<E> implements CustomArrayList<E>{
+
+    public static void main(String[] args) {
+        CustomArrayList<String> strings = new CustomArrayListImpl<>();
+        strings.add("House");
+        strings.add("Car");
+        strings.add("Garage");
+        System.out.println(strings.get(1));
+    }
+    
+    private E[] values;
+
+    public CustomArrayListImpl() {
+        values = (E[]) new Object[0];
+    }
+    
     @Override
     public boolean add(E e){
+        try {
+            E[] temp = values;
+            values = (E[]) new Object[temp.length + 1];
+            System.arraycopy(temp, 0, values, 0, temp.length);
+            values[values.length - 1] = e;
+            return true;
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
         return false;
+        
     }
 
     @Override
@@ -20,8 +44,7 @@ public class CustomArrayListImpl<E> implements CustomArrayList<E>{
 
     @Override
     public E get(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+       return values[index];
     }
 
     @Override
