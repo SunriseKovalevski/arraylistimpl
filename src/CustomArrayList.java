fc07355ef04b28b96bf54b2dbbe13c3924805626
Vladislav Kovalevski <SunriseKovalevsky283@gmail.com>
2024-10-24 16:51:03 +0300

    Added helping  extra methods

--- src/CustomArrayList.java
+++ src/CustomArrayList.java
@@ -1,10 +1,9 @@
-import java.util.Iterator;
 public interface CustomArrayList<E> extends Iterable<E> {
     boolean add(E e);
     boolean addTo(int index, E e);
     E get(int index);
     void delete(int index);
     void clear();
-    void sort();
-
+    int size();
+    E set(int index, E element);
 }
